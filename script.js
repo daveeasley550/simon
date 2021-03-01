30680b700181fd0925fb8814fda248215f8f766c
Dave Easley <easley.david@gmail.com>
2021-03-01 14:17:23 -0500

    working

--- script.js
+++ script.js
@@ -11,41 +11,61 @@ start.addEventListener("click", function () {
     roundStart()
 })
 let roundStart = function () {
-resetButton.addEventListener("click", function(){
-    location.reload()
-})
+    resetButton.addEventListener("click", function () {
+        location.reload()
+    })
     flash = function () {
-        if (computersClicks == 0) {
-            slices[0].style = ""
+        for (let i = 0; i < computersClicks.length; i++) {
+            // slices[computersClicks[i]].style.background = ""
+            // delay(i)
             setTimeout(() => {
-                slices[0].style.background = "none"
-            }, 500)
-        } if (computersClicks == 1) {
-            slices[1].style = ""
+                slices[computersClicks[i]].style.background = ""
+
+            }, i * 1000)
             setTimeout(() => {
-                slices[1].style.background = "none"
-            }, 500)
-        } if (computersClicks == 2) {
-            slices[2].style = ""
-            setTimeout(() => {
-                slices[2].style.background = "none"
-            }, 500)
-        } if (computersClicks == 3) {
-            slices[3].style = ""
-            setTimeout(() => {
-                slices[3].style.background = "none"
-            }, 500)
+                slices[computersClicks[i]].style.background = "none"
+            }, i * 1000 + 300)
+            // if (computersClicks[i] == 0) {
+            //     slices[0].style = ""
+            //     setTimeout(() => {
+            //         slices[0].style.background = "none"
+            //     }, 500)
+            // } if (computersClicks[i] == 1) {
+            //     slices[1].style = ""
+            //     setTimeout(() => {
+            //         slices[1].style.background = "none"
+            //     }, 500)
+            // } if (computersClicks[i] == 2) {
+            //     slices[2].style = ""
+            //     setTimeout(() => {
+            //         slices[2].style.background = "none"
+            //     }, 500)
+            // } if (computersClicks[i] == 3) {
+            //     slices[3].style = ""
+            //     setTimeout(() => {
+            //         slices[3].style.background = "none"
+            //     }, 500)
+            // }
+
         }
+
     }
+    // let delay = function(i){
+    //     setTimeout(() => {
+    //         slices[computersClicks[i]].style.background = "none"
+    //     }, 1000)
+    // }
     for (let i = 0; i < slices.length; i++) {
         slices[i].style.background = "none"
         slices[i].addEventListener("click", function () {
             slices[i].style = ""
             setTimeout(() => {
                 slices[i].style.background = "none"
+                checkLoser()
             }, 500)
+
             playersClicks = i
-            checkLoser()
+
         })
 
     }
@@ -69,7 +89,8 @@ resetButton.addEventListener("click", function(){
 
                 console.log("next round")
             }
-        } else console.log("Fgame over")
+        } else window.alert("Game over"), resetButton.click()
+
     }
 }
 
